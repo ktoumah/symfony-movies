69d6c830d2390d1c6ae2c6fc6c20416d21cef3c3
root <karimtoumah@gmail.com>
2024-07-04 09:36:04 +0000

    style: adding some style

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
