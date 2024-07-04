e72249f33dd984ddd5749b7e7922ad37fc74c8a6
root <karimtoumah@gmail.com>
2024-07-04 00:06:25 +0000

    style: adding style to login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     './templates/**/*.html.twig',
+    './templates/*.html.twig',
     './assets/**/*.js',
-    './src/**/*.php',
   ],
   theme: {
     extend: {},
