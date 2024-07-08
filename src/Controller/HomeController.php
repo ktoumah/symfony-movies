e55b288039080728e090e2046423f22fbbb005f5
root <karimtoumah@gmail.com>
2024-07-08 18:37:48 +0000

    refactor: remove unused code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,11 +12,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(MovieDBAPI $movieDBAPI): Response
     {
-        $trendingMovies = $movieDBAPI->getTrendingMoviesByTimeWindow("day");
-        $id = $trendingMovies['results'][0]['id'];
-
-        $movieDBAPI->getMovieDetails($id);
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
